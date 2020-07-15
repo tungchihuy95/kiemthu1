84183604183e3697dd551c1168f7f5233dc7ef34
tungchihuy95 <tungjpvn95@gmail.com>
2020-07-15 10:36:53 +0700

    jkjlag

---
+++ SimpleCalculator.java
@@ -0,0 +1,13 @@
+public class SimpleCalculator {
+    public static int add(int first, int second){
+        //throw new UnsupportedOperationException("not support operator");
+        return first + second;
+    }
+
+    public static int sub(int first, int second){
+        //throw new UnsupportedOperationException("not support operator");
+        return first - second;
+    }
+}
+
+
